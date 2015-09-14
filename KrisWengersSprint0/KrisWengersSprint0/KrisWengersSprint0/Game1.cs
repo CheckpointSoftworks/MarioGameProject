e12c7fa031e5c451622ccd4b4d0c67e0c8fcca3f
Matt <literatehullmonk@gmail.com>
2015-09-14 23:38:17 +0000

    Matt commit

--- KrisWengersSprint0/KrisWengersSprint0/KrisWengersSprint0/Game1.cs
+++ KrisWengersSprint0/KrisWengersSprint0/KrisWengersSprint0/Game1.cs
@@ -25,6 +25,7 @@ namespace Sprint0
         public Game1()
         {
             //Kris Added this comment and commited it
+            //Matt added this comment and commited it
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
         }
