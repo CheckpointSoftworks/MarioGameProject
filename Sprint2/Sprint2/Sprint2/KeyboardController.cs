6fbc2bb058431ca66d8b40f7f75d7303f456a195
Jonathan Miller <jonny30bass@live.com>
2015-09-22 00:45:35 -0400

    KeyboardController and MushroomSprite images

---
+++ Sprint2/Sprint2/Sprint2/KeyboardController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+
+namespace Sprint2
+{
+    public class KeyboardController : IKeyboard
+    {
+        private Dictionary<Keys, ICommand> controllerMappings;
+
+        public KeyboardController()
+        {
+            controllerMappings = new Dictionary<Keys, ICommand>();
+        }
+
+        public void RegisterCommand(Keys key, ICommand command)
+        {
+            controllerMappings.Add(key, command);
+        }
+
+        public void Update()
+        {
+            Keys[] pressedKeys = Keyboard.GetState().GetPressedKeys();
+
+            foreach (Keys key in pressedKeys)
+            {
+                //Calls the Execute method in the command object corresponding to the pressed key.
+                controllerMappings[key].Execute();
+            }
+        }
+    }
+}
