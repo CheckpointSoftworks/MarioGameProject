3e596c9c63ec35e3e10be097446e76e5aa0170f5
Scott Weddendorf <jeff81193@aol.com>
2015-09-23 01:40:20 -0400

    Command touch ups

---
+++ Sprint2/Sprint2/Sprint2/ResetCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sprint2
+{
+    class ResetCommand:ICommand
+    {
+            private Game1 Game;
+
+            public ResetCommand(Game1 game)
+            {
+                Game = game;
+            }
+
+            public void Execute()
+            {
+                
+            }
+    }
+}
