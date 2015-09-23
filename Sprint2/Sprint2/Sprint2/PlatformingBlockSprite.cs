b912e334f972908539b468dbf6298998b468133b
Ian Weber <weber.595@buckeyemail.osu.edu>
2015-09-23 11:57:26 -0400

    fix cannot begin error

--- Sprint2/Sprint2/Sprint2/PlatformingBlockSprite.cs
+++ Sprint2/Sprint2/Sprint2/PlatformingBlockSprite.cs
@@ -26,10 +26,9 @@ namespace Sprint2
             int spriteSheetSpriteSize = 16;
             Rectangle sourceRectangle = new Rectangle((spriteSheetSpriteSize * 7), 0, spriteSheetSpriteSize, spriteSheetSpriteSize);
             Rectangle destinationRectangle = new Rectangle((int)location.X, (int)location.Y, spriteSheetSpriteSize, spriteSheetSpriteSize);
-
-            
+            spriteBatch.Begin();
             spriteBatch.Draw(platformingBlockSpriteSheet, destinationRectangle, sourceRectangle, Color.White);
-            
+            spriteBatch.End();
         }
     }
 }
