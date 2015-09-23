2959254d8c7ac1040648079150852547cad8771a
Scott Weddendorf <jeff81193@aol.com>
2015-09-23 00:00:50 -0400

    Commands

---
+++ Sprint2/Sprint2/Sprint2/KillMarioCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sprint2
+{
+    public class KillMarioCommand : ICommand
+    {
+        private Game1 Game;
+
+        public KillMarioCommand(Game1 game)
+        {
+            Game = game;
+        }
+
+        public void Execute()
+        {
+            Game.mario.state.Dying();
+        }
+    }
+}
