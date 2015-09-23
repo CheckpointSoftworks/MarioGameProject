3984e801d79b1e28145e45af445f77aff1ca8c45
Kris Wenger <wenger.107@osu.edu>
2015-09-22 23:47:13 -0400

    Pipe class was not uploaded for some reason

---
+++ Sprint2/Sprint2/Sprint2/Pipe.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Sprint2
+{
+    public class Pipe : IGameObject
+    {
+        private ISprite pipeSprite;
+
+        public Pipe()
+        {
+            pipeSprite = new PipeSprite();
+        }
+
+        public void Update()
+        {
+            pipeSprite.Update();
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            pipeSprite.Draw(spriteBatch);
+        }
+    }
+}
