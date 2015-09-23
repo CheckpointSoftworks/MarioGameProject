9d7a5cf6eb0770315ec95e41f1e1816d409971af
Jonathan Miller <jonny30bass@live.com>
2015-09-22 20:08:22 -0400

    Added item objects / empty W.I.P. item sprites

---
+++ Sprint2/Sprint2/Sprint2/SuperMushroom.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Sprint2
+{
+    public class SuperMushroom : IGameObject
+    {
+        private ISprite superMushroomSprite;
+        public SuperMushroom()
+        {
+            superMushroomSprite = new SuperMushroomSprite();
+        }
+        public void Update()
+        {
+            superMushroomSprite.Update();
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            superMushroomSprite.Draw(spriteBatch);
+        }
+    }
+}
---
+++ Sprint2/Sprint2/Sprint2/SuperStar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Sprint2
+{
+    public class SuperStar : IGameObject
+    {
+        private ISprite superStarSprite;
+        public SuperStar()
+        {
+            superStarSprite = new SuperStarSprite();
+        }
+        public void Update()
+        {
+            superStarSprite.Update();
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            superStarSprite.Draw(spriteBatch);
+        }
+    }
+}
