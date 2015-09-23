37da3e448e311a4f8fe6d0f847efd50d2e830bfb
Jonathan Miller <jonny30bass@live.com>
2015-09-22 20:21:28 -0400

    Added BoxCoin object and empty W.I.P. BoxCoinSprite

---
+++ Sprint2/Sprint2/Sprint2/BoxCoin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Sprint2
+{
+    public class BoxCoin : IGameObject
+    {
+        private ISprite boxCoinSprite;
+        public BoxCoin()
+        {
+            boxCoinSprite = new BoxCoinSprite();
+        }
+        public void Update()
+        {
+            boxCoinSprite.Update();
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            boxCoinSprite.Draw(spriteBatch);
+        }
+    }
+}
