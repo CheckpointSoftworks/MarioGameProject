c498ac02d8c4f1f6f27521e2d00a3182c85d1b4a
Scott Weddendorf <jeff81193@aol.com>
2015-09-22 21:53:56 -0400

    Add Mario command classes

---
+++ Sprint2/Sprint2/Sprint2/BigMarioCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sprint2
+{
+    class BigMarioCommand
+    {
+            private Game1 Game;
+
+            public BigMarioCommand(Game1 game)
+            {
+                Game = game;
+            }
+
+            public void Execute()
+            {
+                
+            }
+    }
+}
---
+++ Sprint2/Sprint2/Sprint2/SmallMarioCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sprint2
+{
+    class SmallMarioCommand
+    {
+            private Game1 Game;
+
+            public SmallMarioCommand(Game1 game)
+            {
+                Game = game;
+            }
+
+            public void Execute()
+            {
+                
+            }
+    }
+}
