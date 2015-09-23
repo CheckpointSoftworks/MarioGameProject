fc65f02562488a44319edfa270a2f102ab3de68a
Scott Weddendorf <jeff81193@aol.com>
2015-09-23 00:50:02 -0400

    update to command classes

---
+++ Sprint2/Sprint2/Sprint2/DownCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sprint2
+{
+    class DownCommand: ICommand
+    {
+            private Game1 Game;
+
+            public DownCommand(Game1 game)
+            {
+                Game = game;
+            }
+
+            public void Execute()
+            {
+                
+            }
+    }
+}
