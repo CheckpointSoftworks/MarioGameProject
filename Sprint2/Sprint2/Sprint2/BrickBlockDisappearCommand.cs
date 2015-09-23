593b2f0b786823f1242f5753a8ffc6f0009031aa
Scott Weddendorf <jeff81193@aol.com>
2015-09-22 21:35:04 -0400

    Added block commands

---
+++ Sprint2/Sprint2/Sprint2/BrickBlockDisappearCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sprint2
+{
+    class BrickBlockDisappearCommand
+    {
+            private Game1 Game;
+
+            public BrickBlockDisappearCommand(Game1 game)
+            {
+                Game = game;
+            }
+
+            public void Execute()
+            {
+                Game.brickblock.update();
+            }
+    }
+}
