1f60712a52f30f2f218ab5808f26de7711d38232
Jonathan Miller <jonny30bass@live.com>
2015-09-22 21:08:31 -0400

    Added OneUpMushroomSprite and SuperMushroomSprite

--- Sprint2/Sprint2/Sprint2/OneUpMushroomSprite.cs
+++ Sprint2/Sprint2/Sprint2/OneUpMushroomSprite.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace Sprint2
 {
-    class OneUpMushroomSprite : ISprite
+    public class OneUpMushroomSprite : ISprite
     {
-        //WIP
+        private Texture2D oneUpMushroomSpriteSheet;
+        private Vector2 location;
+        public OneUpMushroomSprite()
+        {
+            oneUpMushroomSpriteSheet = ItemSpriteTextureStorage.CreateOneUpMushroomSprite();
+            location = new Vector2(400, 100);
+        }
+
+        public void Update()
+        {
+            //No update needed for the super mushroom
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+            spriteBatch.Draw(oneUpMushroomSpriteSheet, location, Color.White);
+            spriteBatch.End();
+        }
     }
 }
