f2dc3b41ecffa5587c5b1f0b7cdf25e8a1457a27
Jonathan <jonny30bass@live.com>
2015-10-10 02:36:38 -0400

    Implemented KeyNotPressed class

---
+++ Sprint2/Sprint2/Sprint2/KeyNotPressed.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+
+namespace Sprint2
+{
+    class KeyNotPressed : ICommand
+    {
+        private Game1 game;
+
+        public KeyNotPressed(Game1 gameInstance)
+        {
+            game = gameInstance;
+        }
+
+        public void Execute()
+        {
+            if (Keyboard.GetState().IsKeyUp(Keys.A) && Keyboard.GetState().IsKeyUp(Keys.D)
+                && Keyboard.GetState().IsKeyUp(Keys.W) && Keyboard.GetState().IsKeyUp(Keys.S))
+            {
+                ((Mario)game.mario).State.Still();
+            }
+        }
+    }
+}
