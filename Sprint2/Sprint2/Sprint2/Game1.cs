435453c178adda4a2a2b114b5d16cfd14fb00f95
Jonathan <jonny30bass@live.com>
2015-10-11 01:25:48 -0400

    Merged LeftThumbstickIdle with the KeyNotPressed class. Deleted the LeftThumbstickIdle class as it is not needed anymore.

--- Sprint2/Sprint2/Sprint2/Game1.cs
+++ Sprint2/Sprint2/Sprint2/Game1.cs
@@ -25,9 +25,6 @@ namespace Sprint2
         private Texture2D background;
         private Rectangle mainframe;
         public ICommand keyboardNotPressed;
-        public ICommand leftThumbstickIdle;
-
-
 
         public Game1()
         {
@@ -39,8 +36,7 @@ namespace Sprint2
         {
             keyboard = new KeyboardController();
             gamepad = new GamepadController(this);
-            keyboardNotPressed = new KeyNotPressed(this);
-            leftThumbstickIdle = new LeftThumbstickIdle(this);
+            keyboardNotPressed = new KeyNotPressed(this); 
             loader= new LevelLoader("Level.xml");
             mainframe = new Rectangle(0, 0, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height);
 
@@ -96,7 +92,6 @@ namespace Sprint2
             keyboard.Update();
             gamepad.Update();
             keyboardNotPressed.Execute();
-            leftThumbstickIdle.Execute();
             mario.Update();
             handleCollision();
             foreach (IItemObjects item in loader.staticObjectsList)
