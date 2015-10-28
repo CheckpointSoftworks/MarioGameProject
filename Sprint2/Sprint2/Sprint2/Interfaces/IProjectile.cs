72cbf3b98f6569524392a34b8f7be61ecdbfed11
Scott Weddendorf <jeff81193@aol.com>
2015-10-28 00:59:28 -0400

    Added projectile classes

---
+++ Sprint2/Sprint2/Sprint2/Interfaces/IProjectile.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sprint2
+{
+    public interface IProjectile
+    {
+        void Update();
+
+        void Draw(SpriteBatch spriteBatch);
+
+        void LeftCollision();
+        void RightCollision();
+        void TopCollision();
+        void BottomCollision();
+
+        Rectangle returnCollisionRectangle();
+        Vector2 returnLocation();
+
+    }
+}
