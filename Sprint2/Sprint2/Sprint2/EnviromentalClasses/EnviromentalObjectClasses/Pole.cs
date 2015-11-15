655ebf7d1a6bf98eaa9e15452042ded0e4094aa4
Jonathan Miller <jonny30bass@live.com>
2015-11-14 23:32:35 -0500

    Pole.cs didn't get committed, so I'm committing it now.

---
+++ Sprint2/Sprint2/Sprint2/EnviromentalClasses/EnviromentalObjectClasses/Pole.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Sprint2
+{
+    public class Pole : IPole
+    {
+        private IPoleSprite poleSprite;
+        private Vector2 location;
+        public Pole()
+        {
+            location = new Vector2(UtilityClass.poleLocationX, UtilityClass.poleLocationY);
+            poleSprite = new PoleSprite(location);
+        }
+        public void Update()
+        {
+            poleSprite.Update();
+        }
+        public void Draw(SpriteBatch spriteBatch, Vector2 cameraLoc)
+        {
+            poleSprite.Draw(spriteBatch, cameraLoc);
+        }
+    }
+}
