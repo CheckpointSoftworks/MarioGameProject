278846344501f454e8c8c1fd4da1a1152cfab142
Scott Weddendorf <jeff81193@aol.com>
2015-12-04 23:56:47 -0500

    Added two camera types for Code Reviews

---
+++ Sprint2/Sprint2/Sprint2/CameraClasses/Types/AutoScrollingCameraController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sprint2
+{
+    public class AutoScrollingCameraController
+    {
+        private Camera camera;
+        private IPlayer mario;
+        private Vector2 marioPosition;
+        private int marioCameraPosition;
+        private int scrollSpeed;
+
+        public AutoScrollingCameraController(Camera camera, IPlayer mario, int scrollSpeed)
+        {
+            this.camera = camera;
+            this.mario = mario;
+            this.scrollSpeed = scrollSpeed;
+            marioPosition = ((Mario)mario).Location;
+        }
+
+        public void Update()
+        {
+            marioCameraPosition = (int)(marioPosition.X - camera.GetPosition().X);
+
+            if ((camera.GetPosition().X + UtilityClass.currentScreenMax) < UtilityClass.maxScroll)
+            {
+                camera.MoveRight(scrollSpeed);
+            }
+
+            if (marioCameraPosition < 0)
+            {
+                int newMarioX = (int)marioPosition.X - (marioCameraPosition - scrollSpeed);
+                ((Mario)mario).Location = new Vector2(newMarioX, mario.GetLocation().Y);
+            }
+            if (marioCameraPosition > (UtilityClass.currentScreenMax-16))
+            {
+                int newMarioX = (int)marioPosition.X - (marioCameraPosition - (UtilityClass.currentScreenMax-16));
+               ((Mario)mario).Location = new Vector2(newMarioX, mario.GetLocation().Y);
+            }
+            marioPosition = mario.GetLocation();
+        }
+    }
+}
---
+++ Sprint2/Sprint2/Sprint2/CameraClasses/Types/ClassicCameraController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sprint2
+{
+    public class ClassicCameraController
+    {
+         private int cameraWidth;
+        private Camera camera;
+        private IPlayer mario;
+        private Vector2 marioPosition;
+        private Vector2 cameraPosition;
+        private int screenCenter;
+        private int marioCameraPosition;
+        private int oldMarioCameraPosition;
+
+
+        public ClassicCameraController(Camera camera, IPlayer mario)
+        {
+            this.camera = camera;
+            cameraWidth = camera.GetWidth();
+            cameraPosition = camera.GetPosition();
+            this.mario = mario;
+            marioPosition = mario.GetLocation();
+            screenCenter = cameraWidth/UtilityClass.two;
+        }
+
+        public void Update()
+        {
+            marioCameraPosition = (int)(marioPosition.X - cameraPosition.X);
+
+            if (marioCameraPosition < screenCenter && marioCameraPosition > (screenCenter/2) && (camera.GetPosition().X + UtilityClass.currentScreenMax) < UtilityClass.maxScroll)
+            {
+                if (marioCameraPosition > oldMarioCameraPosition) {
+                    camera.MoveRight(1);
+                }
+            }
+
+            if (marioCameraPosition > screenCenter && (camera.GetPosition().X + UtilityClass.currentScreenMax) < UtilityClass.maxScroll)
+            {
+                camera.MoveRight(marioCameraPosition - screenCenter);
+            }
+
+            if (marioCameraPosition < 0)
+            {
+                int newMarioX = (int)marioPosition.X - marioCameraPosition;
+                ((Mario)mario).Location = new Vector2(newMarioX, mario.GetLocation().Y);
+            }
+            oldMarioCameraPosition = marioCameraPosition;
+            cameraPosition = camera.GetPosition();
+            marioPosition = mario.GetLocation();
+        }
+    }
+}
