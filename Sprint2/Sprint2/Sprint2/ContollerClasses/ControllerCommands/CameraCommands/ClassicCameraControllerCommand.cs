e5e73ff3d3ccad2503b37124204676f569525177
Scott Weddendorf <jeff81193@aol.com>
2015-12-06 19:01:15 -0500

    Added Camera Commands

---
+++ Sprint2/Sprint2/Sprint2/ContollerClasses/ControllerCommands/CameraCommands/ClassicCameraControllerCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sprint2
+{
+    public class ClassicCameraControllerCommand:ICommand
+    {
+        private Game1 game;
+
+        public ClassicCameraControllerCommand(Game1 game)
+        {
+            this.game = game;
+        }
+        public void Execute()
+        {
+            game.cameraController = new ClassicCameraController(game.camera, game.mario);
+        }
+    }
+}
